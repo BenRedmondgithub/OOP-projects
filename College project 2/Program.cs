afe137f61da84ba500b01d14c40f764cf16861d7
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-02-14 12:45:26 +0000

    Updated project file

---
+++ College project 2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+namespace ShapePro
+{
+
+    public class Shape 
+    
+    {
+        protected String Name;
+
+        public Shape (string Name) 
+        {
+            this.Name = Name;
+        }
+
+    public virtual void DrawShape() 
+    {
+        Console.WriteLine($"Drawing a {Name}...");
+    }
+
+    public virtual double CalculateArea() 
+    {
+        Console.WriteLine("Calculating area...");
+        return 0;
+    }
+
+    public class Triangle : Shape 
+    {
+        protected double SideA, SideB, SideC;
+
+        public Triangle (string name, double sideA, double sideB, double sideC) : base (name)
+        
+        {
+            this.SideA = sideA;
+            this.SideB = sideB;
+            this.SideC = sideC;
+        }
+ 
+            public override void DrawShape()
+        {
+            Console.WriteLine($" Drawing {Name} with side{SideA}, {SideB}, {SideC} ... ");
+        }
+
+        }
+
+    public class RightAngledTriangle : Triangle 
+        {
+            public RightAngledTriangle(Double sideA, double SideB) : base ("Right Angled Triangle",double sideA, double SideB)
+            {}
+        }
+
+    public class EqiulateralTriangle : Triangle 
+        {
+
+        }
+
+
+
+    }
+
+    class Mainprogram 
+    {
+
+        public static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
